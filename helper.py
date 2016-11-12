32a023391cf778e7c8541aed1c9aceb4c0e20aa2
Dan Aharoni <aharonidan@gmail.com>
2016-11-12 12:42:08 +0000

    add kelvin to celsius function

--- helper.py
+++ helper.py
@@ -3,25 +3,29 @@ from datetime import datetime
 
 attributes = {
     'description': lambda item: item['weather'][0]['description'],
-    'temperature': lambda item: item['main']['temp'],
-    'pressure': lambda item: item['main']['pressure'],
-    'humidity': lambda item: item['main']['humidity']
+    'temperature': lambda item: kelvin_to_celsius(item['main']['temp']),
+    'pressure': lambda item: str(item['main']['pressure']),
+    'humidity': lambda item: str(item['main']['humidity']) + '%'
 }
 
-def parse_forecast_json(filename):
+def kelvin_to_celsius(kelvin):
+    celsius = round(kelvin - 273.15)
+    return str('%gC' % celsius)
+
+def parse_json(filename):
     with open(filename) as file:
         json_data = json.load(file)
 
     result = {}
     for item in json_data['list']:
         date = item['dt_txt']
-            result[date] = {}
+        result[date] = {}
         for attribute, get_attribute in attributes.items():
             result[date][attribute] = get_attribute(item)
 
     return result
 
-def fetch_data(forecast, date, time, attribute=None):
+def fetch_data(forecast, date, time, attribute = None):
 
     date_time = format_date(date, time)
 
@@ -37,10 +41,8 @@ def fetch_data(forecast, date, time, attribute=None):
     return result
 
 def error_response(date_time):
-    return {"status": "error", "message": "No data for " + date_time}
+    return { 'status': 'error', 'message': 'No data for ' + date_time }
 
 def format_date(date, time):
-    date = datetime.strptime(date + time, '%Y%m%d%H%M')
-    return str(date)
-
-
+    result = datetime.strptime(date + time, '%Y%m%d%H%M')
+    return str(result)
